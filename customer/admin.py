cc8d0fe7032aab9740bbddc78a14d2a2f9554f4e
AliaksandrSihai <sigai.aleksandr@mail.ru>
2024-04-20 16:06:14 +0300

    feature(/.): add admin panel for all apps

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from customer.models import Customer
 
 # Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    """Админка для работы с заказчиком"""
+    list_display = ('name', 'surname', 'phone', 'email', 'experience')
+    search_fields = ('email',)
+    ordering = ('name',)
--- performer/admin.py
+++ performer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from performer.models import PerformerModel
 # Register your models here.
+@admin.register(PerformerModel)
+class PerformerModelAdmin(admin.ModelAdmin):
+    """Админка для работы с исполнителем"""
+    list_display = ('name', 'surname', 'phone', 'email', 'experience')
+    search_fields = ('email',)
+    ordering = ('name',)
